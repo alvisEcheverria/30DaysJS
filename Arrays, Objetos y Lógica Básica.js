b9332d160deb0c7afdb8b65154fb9cdce17b8807
alvisEcheverria <alvis.cm@gmail.com>
2025-08-26 22:57:53 +0200

    Dia 2: Hacer un objeto “usuario” con nombre, edad y hobbies; imprimir sus propiedades con destructuring, deep freeze añadido / done

--- Arrays, Objetos y Lógica Básica.js
+++ Arrays, Objetos y Lógica Básica.js
@@ -15,4 +15,46 @@ const MIN = Math.min(...number);
 //Promedio: 
 const average = number.reduce((acc, el) => acc + el, 0) / number.length; 
 
-console.log({ MAX, MIN, average })
+console.log({ MAX, MIN, average });
+
+//Día 2 : Hacer un objeto “usuario” con nombre, edad y hobbies; imprimir sus propiedades con destructuring.
+
+const usuario = {
+    nombre: "Alvis Echeverria",
+    edad: 34,
+    hobbies: ["nadar", "ver anime", "gimnasio"]
+};
+
+const { nombre: name, edad = 23, hobbies } = usuario;
+
+const [ primero = "loose", segundo = "loose", tercero = "loose", ...rest ] = usuario.hobbies;
+
+usuario.direccion = { ciudad: "anywhere" };
+
+const deepFreeze = obj => {
+    Object.keys(obj).forEach(prop => {
+        if(typeof obj[prop] === "object" && !Object.isFrozen(obj[prop])){
+            deepFreeze(obj[prop]);
+        }
+    });
+    return Object.freeze(obj);
+};
+
+deepFreeze(usuario);
+
+console.log({
+    name,
+    edad,
+    hobbies: {
+        primero,
+        segundo,
+        tercero,
+    },
+    hobbiesMap: [...hobbies]
+});
+
+function presentarUsuario({ nombre = "unnamed", edad = 23, hobbies =  [] }) {
+    return `${ nombre } (${ edad }) — hobbies: ${ hobbies?.length ? hobbies.join(", ") : "loose" }`
+};
+
+console.log(presentarUsuario(usuario));
